8e86b44102e3f97d2b511999c9e64cbef34d5adc
Ryan Meneses <meneses.ryan.j@gmail.com>
2021-01-19 12:22:24 -0800

    Add general exception.

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -229,7 +229,7 @@ class WebServer {
               builder.append("\n");
               builder.append("Arguments not accepted.");
             }
-          } catch (TaskExecutionException ex) {
+          } catch (Exception ex) {
               builder.append("HTTP/1.1 400 Bad Request\n");
               builder.append("Content-Type: text/html; charset=utf-8\n");
               builder.append("\n");
