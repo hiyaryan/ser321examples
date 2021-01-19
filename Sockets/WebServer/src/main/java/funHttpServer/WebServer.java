5e75ec91a1cf4f424792f83a5b9ab1ad405436e1
Ryan Meneses <meneses.ryan.j@gmail.com>
2021-01-19 13:42:34 -0800

    Add github? implementation

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -25,6 +25,7 @@ import java.util.Random;
 import java.util.Map;
 import java.util.LinkedHashMap;
 import java.nio.charset.Charset;
+import org.json.*;
 
 class WebServer {
   public static void main(String args[]) {
@@ -250,13 +251,28 @@ class WebServer {
           String json = fetchURL("https://api.github.com/" + query_pairs.get("query"));
           System.out.println(json);
 
-          builder.append("Check the todos mentioned in the Java source file");
           // TODO: Parse the JSON returned by your fetch and create an appropriate
           // response
           // and list the owner name, owner id and name of the public repo on your webpage, e.g.
           // amehlhase, 46384989 -> memoranda
           // amehlhase, 46384989 -> ser316examples
           // amehlhase, 46384989 -> test316
+
+          JSONArray jsonArray = new JSONArray(json);
+
+          for(int i = 0; i < jsonArray.length(); i++) {
+            String repo_name = jsonArray.getJSONObject(i).getString("name");
+            String owner_login = jsonArray.getJSONObject(i).getJSONObject("owner").getString("login");
+            String owner_id = jsonArray.getJSONObject(i).getJSONObject("owner").getString("id");
+
+            builder.append("HTTP/1.1 200 OK\n");
+            builder.append("Content-Type: text/html; charset=utf-8\n");
+            builder.append("\n");
+            builder.append(owner_login + ", " + owner_id + " -> " + repo_name);
+          }
+
+          builder.append("HTTP/1.1 200 OK\n");
+          builder.append("Content-Type: text/html; charset=utf-8\n");
 
         } else {
           // if the request is not recognized at all
